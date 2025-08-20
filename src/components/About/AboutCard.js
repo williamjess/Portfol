87d2640683bd44c22c7c1b3bd90d7d9d6208da7f
William <140663643+williamjess@users.noreply.github.com>
2025-08-20 21:33:16 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -5,22 +5,36 @@ import { ImPointRight } from "react-icons/im";
 function AboutCard() {
   return (
     <Card className="quote-card-view">
-      
       <Card.Body>
-        
         <blockquote className="blockquote mb-0">
-          <h3 style={{ textAlign: "justify",  }}>
-          
-            
-          <br /> 
- 
-          <br />
-           <p> Hi, I'm <span className="purple">William Jess </span>
-            from <span className="purple"> Belfast, Northern Ireland.</span> </p>
-            <br />    
-            <p>I'm a Computer Science graduate from Ulster University, and I'm currently open to full-time roles at Junior or Entry-Level.</p>
+          <h3 style={{ textAlign: "justify" }}>
             <br />
-            Feel free to review my tech stack below and visit my <a href="https://github.com/williamjess" ><span className="purple"> Github</span> profile to assess my fit for any role.
+
+            <p>
+              Hi, I'm <span className="purple">William Jess</span> from{" "}
+              <span className="purple">Belfast, Northern Ireland.</span>
+            </p>
+
+            <br />
+
+            <p>
+              I'm a Computer Science graduate from Ulster University, and I'm
+              currently open to full-time roles at Junior or Entry-Level.
+            </p>
+
+            <br />
+
+            <p>
+              Feel free to review my tech stack below and visit my{" "}
+              <a
+                href="https://github.com/williamjess"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="purple">Github</span>
+              </a>{" "}
+              profile to assess my fit for any role.
+            </p>
           </h3>
         </blockquote>
       </Card.Body>
@@ -29,3 +43,4 @@ function AboutCard() {
 }
 
 export default AboutCard;
+
